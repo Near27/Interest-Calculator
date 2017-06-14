6d0ab0daedcc903991d040d45139125416bfb817
stefan.tudose@kcl.ac.uk <stefan.tudose@kcl.co.uk>
2017-06-14 20:17:06 +0100

    fetch currency rates  from backend

--- client/src/Result.js
+++ client/src/Result.js
@@ -1,52 +1,88 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Result extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.data,
-      total: props.total,
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencies: ['gbp', 'usd', 'eur'],
+            rates: [],
+        }
+        this.fetchRates = this.fetchRates.bind(this);
+        this.exchange = this.exchange.bind(this);
     }
-  }
-  
-  render() {
-    return (
-        <div className="object-wrapper padding">
-            <label>Result</label>
-            <p>{this.state.data}</p>
-            <table className="result">
-                <thead>
-                    <tr>
-                    <th>Y/M</th>
-                    <th>Interest</th>
-                    <th>Balance</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                    <tr>
-                    <td></td>
-                    <td>Total interest:</td><td>{this.state.total}</td>
-                    </tr>
-                </tfoot>
-                <TableRows data={this.state.data} />
-            </table>
-        </div>
-    );
-  }
+
+    fetchRates(e) {
+        let url = 'http://localhost:3001/calculate/' + this.props.currency;
+        console.log(url);
+        axios.get(url)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    rates: res.data,
+                })
+                
+            })
+            .catch((err) => {
+                console.log('Error');
+            })
+    }
+
+    exchange(e) {
+        console.log(e.target.value);
+        this.fetchRates();
+        this.setState({total: parseFloat(this.state.total) * this.state.rates[0]})
+    }
+
+    render() {
+        return (
+            <div className="object-wrapper padding">
+                <label>Result</label>
+                <p>{this.state.rates}</p>
+                <p>{this.props.currency}</p>
+                <table className="result">
+                    <thead>
+                        <tr>
+                            <th>Y/M</th>
+                            <th>Interest</th>
+                            <th>Balance</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <td>
+                                <select 
+                                    onChange={this.fetchRates}
+                                    type="dropdown"
+                                    ref="currency"
+                                    placeholder="Choose currency">
+                                    <option value="gbp">Pound</option>
+                                    <option value="usd">Dollar</option>
+                                    <option value="eur">Euro</option>
+                                </select>
+                            </td>
+                            <td>Total interest: </td><td>{this.props.total}</td>
+                        </tr>
+                    </tfoot>
+                    <TableRows interest={this.props.interest} balance={this.props.balance} />
+                </table>
+            </div>
+        );
+    }
 }
 
 const TableRows = (props) => (
     <tbody>
-    { props.data.map((row, index) => {
+        { props.interest.map((row, index) => {
             return (
                 <tr key={index}>
-                    <td>{index}</td>
+                    <td>{index + 1}</td>
                     <td>{row}</td>
-                    <td>{row}</td>
+                    <td>{props.balance[index]}</td>
                 </tr>
-                )
+            )
         })
-    } 
+        }
     </tbody>
 )
 
--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ const app = express();
 
 app.use(cors());
 
-const stuff = ['1', '2', '3']
+const gbp = ['0.49246', '0.69714'];
+const usd = ['2.03032', '1.41544'];
+const eur = ['1.43448', '0.70641'];
 
-app.get('/calculate', (req, res) => {
-    res.send(stuff);
+app.get('/calculate/:currency', (req, res) => {
+    if(req.params.currency.match('gbp')) {
+        res.send(gbp);
+    } else if(req.params.currency.match('usd')) {
+        res.send(usd);
+    } else {
+        res.send(eur);
+    }
 })
 
 app.listen(3001, function() {
