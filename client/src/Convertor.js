97232c859e8b1137ec4b69d5021cb3d8a5344660
stefan.tudose@kcl.ac.uk <stefan.tudose@kcl.co.uk>
2017-06-15 20:36:59 +0100

    separate component for the currency convertor

---
+++ client/src/Convertor.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Convertor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencies: ['gbp', 'usd', 'eur'],
+            rates: [],
+            total: 0,
+            totalCurrency: '',
+            totalConverted: 0,
+            convertedCurrency: '',
+        }
+        this.fetchRates = this.fetchRates.bind(this);
+        this.exchange = this.exchange.bind(this);
+        this.fetchRates();
+    }
+
+    render() {
+        return (
+            <div className="row-c-space">
+                <h1>Total: {this.props.total.toFixed(2) }{this.getLabel(this.props.currency) }</h1>
+                <hr className="convertorDivider" />
+                <h1>{ this.state.totalConverted.toFixed(2) }</h1>
+                <select
+                    onChange={this.exchange}
+                    type="dropdown"
+                    ref="currency">
+                    {this.state.currencies.map((c, index) => <option key={index} value={c}>{c}</option>) }
+                </select>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.setState({
+            totalCurrency: this.props.currency,
+            currencies: this.state.currencies.filter(c => c !== this.props.currency),
+        })
+    }
+
+    exchange(e) {
+        this.fetchRates();
+        this.setState({
+            totalConverted: this.props.total * this.state.rates[this.state.currencies.indexOf(e.target.value)],
+            convertedCurrency: e.target.value,
+        })
+    }
+
+    fetchRates(e) {
+        let url = 'http://localhost:3001/calculate/' + this.props.currency;
+        axios.get(url)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    rates: res.data,
+                })
+            })
+            .catch((err) => {
+                console.log('Error');
+            })
+    }
+
+    getLabel(input) {
+        switch (this.props.currency) {
+            case 'gbp':
+                return <label>&#163; </label>
+            case 'usd':
+                return <label>&#36; </label>
+            case 'eur':
+                return <label>&#8364; </label>
+            default:
+                break;
+        }
+    }
+}
+
+export default Convertor;
--- client/src/Result.js
+++ client/src/Result.js
@@ -1,35 +1,20 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Convertor from './Convertor';
 
 class Result extends Component {
     constructor(props) {
         super(props);
         this.state = {
             currencies: ['gbp', 'usd', 'eur'],
-            rates: [],
             total: 0,
-            totalConverted: 0,
-            convertedCurrency: '',
         }
-        this.fetchRates = this.fetchRates.bind(this);
-        this.exchange = this.exchange.bind(this);
-        this.fetchRates();
     }
 
     render() {
         return (
             <div className="object-wrapper padding">
-            <label>Change currency</label>
-                <div className="row-c-space">
-                    <h1>{this.state.total.toFixed(2)}{this.getLabel(this.props.currency)}</h1>
-                    <select 
-                            onChange={this.exchange}
-                            type="dropdown"
-                            ref="currency">
-                            {this.state.currencies.map((c, index) => <option key={index} value={c}>{c}</option>)}
-                    </select>
-                    <h1>{this.state.totalConverted.toFixed(2)}</h1>
-                </div>
+                <label>Change currency</label>
+                <Convertor currency={this.props.currency} total={this.state.total} />
                 <table className="result">
                     <thead>
                         <tr>
@@ -42,7 +27,7 @@ class Result extends Component {
                         <tr>
                             <td></td>
                             <td>Total interest: </td>
-                            <td>{this.state.total.toFixed(2)}{this.getLabel(this.props.currency)}</td>
+                            <td>{this.state.total.toFixed(2) }{this.getLabel(this.props.currency) }</td>
                         </tr>
                     </tfoot>
                     <TableRows interest={this.props.interest} balance={this.props.balance} />
@@ -51,48 +36,19 @@ class Result extends Component {
         );
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let total = this.props.balance[this.props.balance.length - 1];
-        this.setState({
-            total: total,
-            totalConverted: total,
-            currencies: this.state.currencies.filter(c => c !== this.props.currency),
-        })
-    }
-
-    exchange(e) {
-        this.setState({totalConverted: this.state.total * this.state.rates[this.state.currencies.indexOf(e.target.value)],
-            convertedCurrency: e.target.value,
-
-                        })
-    }
-    
-    fetchRates(e) {
-        let url = 'http://localhost:3001/calculate/' + this.props.currency;
-
-        axios.get(url)
-            .then((res) => {
-                console.log(res.data);
-                this.setState({
-                    rates: res.data,
-                })
-            })
-            .catch((err) => {
-                console.log('Error');
-            })
+        this.setState({total: total})
     }
 
     getLabel(input) {
         switch (this.props.currency) {
             case 'gbp':
-                return <label>&#163;</label>
-                break;
+                return <label>&#163; </label>
             case 'usd':
-                return <label>&#36;</label>
-                break;
+                return <label>&#36; </label>
             case 'eur':
-                return <label>&#8364;</label>
-                break;
+                return <label>&#8364; </label>
             default:
                 break;
         }
@@ -105,11 +61,11 @@ const TableRows = (props) => (
             return (
                 <tr key={index}>
                     <td>{index + 1}</td>
-                    <td>{row.toFixed(2)}</td>
-                    <td>{props.balance[index].toFixed(2)}</td>
+                    <td>{row.toFixed(2) }</td>
+                    <td>{props.balance[index].toFixed(2) }</td>
                 </tr>
             )
-          })
+        })
         }
     </tbody>
 )
